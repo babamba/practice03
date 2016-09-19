e24d59bd11b8dbeec1d1d9d4e2af32ce4ef6c6f0
bit-user <orochi13@naver.com>
2016-09-19 17:41:44 +0900

    f

---
+++ src/prob4/Add.java
@@ -0,0 +1,33 @@
+package prob4;
+
+public class Add {
+	private int a;
+	private int b;
+	
+	
+	void setValue(int a, int b){
+		this.a = a;
+		this.b = b;
+	}
+	
+	int calculate(){
+		return this.a + this.b;
+	}
+	
+}
+
+	
+
+	//class add{
+//	int i
+//	int r
+//	setValue(int i, int r)
+	
+//	switch(operator){
+//	case 1 : "+";
+//	add a = new Add();
+//	a.Setvalue(i,r);
+	
+	
+
+
---
+++ src/prob4/Div.java
@@ -0,0 +1,17 @@
+package prob4;
+
+public class Div {
+	private int a;
+	private int b;
+	
+	
+	void setValue(int a, int b){
+		this.a = a;
+		this.b = b;
+	}
+	
+	int calculate(){
+		return this.a / this.b;
+	}
+	
+}
